7540ec5d9f7f8204c9123edfc8492da0bcee5219
Adijav <us-guest@us-guests-MacBook-Pro.local>
2020-02-05 17:02:14 -0600

    PWA functionality

---
+++ public/worker.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = 'pwa-task-manager';
+var urlsToCache = [
+  '/',
+  '/completed',
+  'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
+  '/static/js/bundle.js',
+  '/static/js/0.chunk.js',
+  '/static/js/main.chunk.js'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
