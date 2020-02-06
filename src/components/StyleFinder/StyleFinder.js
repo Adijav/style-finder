c6f879de5ec37c08f1eaff8fc923bae0a8a124e1
Adijav <us-guest@us-guests-MacBook-Pro.local>
2020-02-06 12:54:27 -0600

    updated code

--- src/components/StyleFinder/StyleFinder.js
+++ src/components/StyleFinder/StyleFinder.js
@@ -1,7 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react';
 import Container from '@material-ui/core/Container';
-import {alphabets} from '../../constants';
-import {styleNames} from '../../constants';
 import { Button } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
@@ -12,20 +10,21 @@ const StyleFinder = () => {
     const classes = useStyles();
     const scrollContainer = useRef(null);
     const styleAlphabetRef = useRef(Array.from({length: 27}, () => React.createRef()));
+    
     const [showBackToTopIcon, setBackToTopIcon] = useState(false);
-    let styleMapper = {};
+
+    const [styleMapper, setStyleMapper] = useState({});
 
     useEffect(()=> {
+        const tempObj = {};
         axios.get('/styles.json')
         .then(response => {
             response.data.forEach((style)=>{
-                styleMapper[style.charAt(0).toUpperCase()] ? styleMapper[style.charAt(0).toUpperCase()].push(style) : styleMapper[style.charAt(0).toUpperCase()] = [style];
+                tempObj[style.charAt(0).toUpperCase()] ? tempObj[style.charAt(0).toUpperCase()].push(style) : tempObj[style.charAt(0).toUpperCase()] = [style];
             });
-            console.log(styleMapper);
-
+            setStyleMapper(tempObj);
         })
     },[]);
-    
     const handleScroll = () => {
         scrollContainer.current.scrollTop ? setBackToTopIcon(true) : setBackToTopIcon(false);
     }
@@ -33,33 +32,30 @@ const StyleFinder = () => {
     const handleBackToTop = () => {
         scrollContainer.current.scrollTop = 0;
     }
-
+    
     const handleAlphabetClick = (event) => {
         const targetId = event.currentTarget.id;
         const targetIndex = targetId.split('-');
         scrollContainer.current.scrollTop = styleAlphabetRef.current[targetIndex[1]].current.offsetTop;
     }
-
-    const renderAlphabets = Object.keys(styleMapper).map((alphabet,i) => (
-        <Button id={`alpha-${i}`} onClick={handleAlphabetClick} key={i} className={classes.alphabet} >{alphabet}</Button>
-    ));
-
-    const renderStyles = styleNames.map((style,i) => (
-        <React.Fragment key={i}>
-        <div ref={styleAlphabetRef.current[i]} id={`alphabet-${i}`}>{style.alphabet}</div>
-        <Divider/>
-        {style.styles.map((name,index) => (
-            <div key={index}>{name}</div>
-        ))}
-        </React.Fragment>
-    ));
+  
     return (
         <Container className={classes.container} maxWidth="sm">
             <div className="alphabets">
-                {renderAlphabets}
+                {styleMapper && Object.keys(styleMapper).map((alphabet,i) => (
+                    <Button id={`alpha-${i}`} onClick={handleAlphabetClick} key={i} className={classes.alphabet} >{alphabet}</Button>
+                ))}
             </div>
-            <div id="test" ref={scrollContainer} className={classes.styleNames} onScroll={handleScroll}>
-                {renderStyles}
+            <div ref={scrollContainer} className={classes.styleNames} onScroll={handleScroll}>
+                {styleMapper && Object.keys(styleMapper).map((style,i) => (
+                    <React.Fragment key={i}>
+                    <div ref={styleAlphabetRef.current[i]} id={`alphabet-${i}`}>{style}</div>
+                    <Divider/>
+                    {styleMapper[style].map((name,index) => (
+                        <div key={index}>{name}</div>
+                    ))}
+                    </React.Fragment>
+                ))}
             </div>
             {showBackToTopIcon && <Button onClick={handleBackToTop} className={classes.arrowButton}>
                 <ArrowUpwardIcon className={classes.arrowUpIcon}/>
